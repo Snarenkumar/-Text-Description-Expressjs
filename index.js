3e5391c2889a4ae0bb6aeec8134a771063ceb6db
S Naren Kumar <vssnaren@gmail.com>
2025-01-06 09:38:55 +0530

    started with a new project and intialized a empty express file

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-import express from "express"
-
-
+import express from "express";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+let dotenv = require('dotenv').config()
 const app = express();
-const port =3010 ;
+const port = 3010;
+const pass = dotenv;
 
+const createHash = async () => {
+    const hash = await bcrypt.hash(pass, 10); // Use 10 salt rounds
+    console.log(hash);
+};
+createHash();
 
-app.get("/",(req,res)=>{
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-    res.render("index.ejs")
-})
+// Template engine setup
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
+// Routes
+app.post("/submit", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.send("Sent successfully");
+});
 
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
 
-app.listen(port ,()=>
-{
-    console.log(`server is hosted in this port : ${port}`)
-})
+// Start server
+app.listen(port, () => {
+    console.log(`Server is hosted on port: ${port}`);
+});
