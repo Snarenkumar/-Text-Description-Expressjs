3056ec588db04b505a47144994fd06a9775c2619
S Naren Kumar <vssnaren@gmail.com>
2025-01-06 22:44:53 +0530

    all updated one with text prompt conversion integreated 0

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const cleanResponse = (responseText) => {
         .trim();
 };
 
-// Functisdsfson to generate voiceover content
+// Functisdsfson to generate voicsaeover content
 const generateVoiceoverContent = async (prompt) => {
     try {
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
