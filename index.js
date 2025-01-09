a3e417b66616ac29dc3cbd64159538f7fa882ca4
S Naren Kumar <vssnaren@gmail.com>
2025-01-09 22:09:53 +0530

    0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
-// Load environsadasmenasdsast variaasdasbles
+// Load environsadasmenasast variables
 dotenv.config({ path: './.env.local' });
 const time = 180 ;
 // Import Google Generative AI module
