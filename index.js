5ec6de2c888d15a9b6914b31b3122b2a89c6a288
S Naren Kumar <vssnaren@gmail.com>
2025-01-11 23:11:13 +0530

    working with the index1,js file from the server side  0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
-// LoDSFad environSG`meSADnt varSDFiabJsdaHledsfs
+// LoDSFad environSG`meSADnt varSDFiabJsdaHles
 dotenv.config({ path: './.env.local' });
 const time = 180 ;
 // Import Google GenerDSFSDative AI moWEFdsdfule
