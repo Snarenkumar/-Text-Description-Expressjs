d69a87d61814d1b0b1d9884a778561ab8f3dd443
S Naren Kumar <vssnaren@gmail.com>
2025-01-11 23:09:12 +0530

    working with the index1,js file from the server side  0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
-// Load environment variables
+// Load environSG`ment variables
 dotenv.config({ path: './.env.local' });
 const time = 180 ;
 // Import Google Generative AI module
