6df08cdc934defcea850c88025358f1db815f648
S Naren Kumar <vssnaren@gmail.com>
2025-01-13 23:06:52 +0530

    initailize the file 0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
-// Load environmasdent varisadables
+// Loasdad environmasdent varisadables
 dotenv.config({ path: './.env.local' });
 const time = 180 ;
 // Import Google Generative AI module
