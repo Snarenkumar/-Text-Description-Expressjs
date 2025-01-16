c401462860e36e4643143fdc6fa314262644bc72
S Naren Kumar <vssnaren@gmail.com>
2025-01-16 19:35:19 +0530

    0

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (!apiKey) {
     process.exit(1);
 }
 
-// Iasnasditqwializeqw Google Genwqqweratiqsavwqe AI
+// Iasnasditqwializeqw Google Genwqqweratiqsavwqe AIsa
 const genAI = new GoogleGenerativeAI(apiKey);
 
 // Function to return cleaned response
